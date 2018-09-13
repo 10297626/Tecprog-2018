d1faae5eb4e375d13baed547d0d7bc5cbd40af37
Henrique Suzuki <32173626+10297626@users.noreply.github.com>
2018-09-13 13:56:27 -0300

    Update and rename hashtable.h to htable.h

--- htable.h
+++ htable.h
@@ -1,10 +1,12 @@
-
 /*//////////////////////////////////////////////////////////////////////
 Nome: Henrique Suzuki                 NUSP: 10297626
 Nome:                                 NUSP:         
 Nome:                                 NUSP:         
 //////////////////////////////////////////////////////////////////////*/
 
+#include "advlib.h"
+
 typedef struct{
-  
+  elemento ele;
+  int key;
 }hItem;
