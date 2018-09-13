34d3003cdb961b4579ce74d0f203e8f3362ad499
Henrique Suzuki <32173626+10297626@users.noreply.github.com>
2018-09-13 14:35:56 -0300

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -4,3 +4,23 @@ typedef struct{
   elemento ele;
   struct node next;
 }node;
+
+typedef struct{
+  node head;
+  int count;
+  char *nome;
+}lista
+
+lista cria();
+
+void destroi(lista l);
+
+lista insere(lista l, elemento *val);
+
+elemento busca(lista l, char *n);
+
+elemento retira(lista l, elemento  *val);
+
+
+  
+  
