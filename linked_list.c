830544cb8994da652c50b403d56bf42d23e56a7f
Henrique Suzuki <32173626+10297626@users.noreply.github.com>
2018-09-13 15:02:28 -0300

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -6,10 +6,43 @@ lista cria(){
   lista *l= (lista*)malloc(sixeof(lista));
   l->count=0;
   l->head=null;
+  l->last=null;
+  return l;
 }
 
 void destroi(lista *l){
   free(l);
 }
 
+lista insere(lista l, elemeento *val, char *n){
+  node new;
+  new->ele=val;
+  new->next=null;
+  new->nome=n;
+  
+  if (l->head==null){
+    l->head=new;
+    l->last=new;
+  }else{
+    l.last->next=new;
+    l.last=new;
+  }    
+}
 
+elemento busca(lista l, char *n){
+  node atual;
+  atual=l.head
+  if(atual.nome==n){
+    return head.ele;
+  }
+  while(atual.next!=null){
+    atual = head.next;
+    if (atual.nome==n){
+      return atual.ele;
+    }
+  }
+  return null;      
+ }
+  
+  
+}
