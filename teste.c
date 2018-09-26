d7f302629d844c2085592a84e7cffed4f13f2de5
Henrique Suzuki <32173626+10297626@users.noreply.github.com>
2018-09-26 08:51:29 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -4,8 +4,8 @@ Nome: Henrique Suzuki                 NUSP: 10297626
 Nome: Rubens Gomes Neto               NUSP: 9318484
 //////////////////////////////////////////////////////////////////////*/
 
-#include "stdio.h"
-#include "string.h"
+#include <stdio.h>
+#include <string.h>
 #include "advlib.h"
 #include "hash_table.h"
 #include "linked_list.h"
