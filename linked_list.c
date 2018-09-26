e79272b216353df42aef683007278d0d0762f461
Henrique Suzuki <32173626+10297626@users.noreply.github.com>
2018-09-26 08:29:17 -0300

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -1,3 +1,9 @@
+/*//////////////////////////////////////////////////////////////////////
+Nome: Alessandro Bezerra da Silva     NUSP: 10284351
+Nome: Henrique Suzuki                 NUSP: 10297626
+Nome: Rubens Gomes Neto               NUSP: 9318484
+//////////////////////////////////////////////////////////////////////*/
+
 #include "stdio.h"
 #include "linked_list.h"
 #include "advlib.h"
