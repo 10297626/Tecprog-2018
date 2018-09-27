d71af506701437e6ba01d27b871b194dab573cfd
Henrique Suzuki <32173626+10297626@users.noreply.github.com>
2018-09-27 15:17:00 -0300

    Update hash_table.h

--- hash_table.h
+++ hash_table.h
@@ -4,25 +4,33 @@ Nome: Henrique Suzuki                 NUSP: 10297626
 Nome: Rubens Gomes Neto               NUSP: 9318484
 //////////////////////////////////////////////////////////////////////*/
 
+//#include guards
+#ifndef hashtable_h
+#define hashtable_h
+
 #include "advlib.h"
 
+
+
 typedef struct simbolo{
-  elemento ele;
-  char* key;
+  elemento *ele;
+  char *key;
 }simbolo;
 
 typedef struct tabsim{
   int size;
-  simbolo **tab;
+  int count;
+  simbolo** simbolos;
 }tabsim;
 
-tabsim ht_cria(int tam);
+tabsim *ht_cria(int tam);
 
-void ht_destroi(tabsim t);
+void ht_destroi(tabsim *tab);
 
-int ht_insere(tabsim t, char *n,elemento *val);
+int ht_insere(tabsim *tab, char *key,elemento *ele);
 
-elemento ht_busca(tabsim t, char *n);
+elemento* ht_busca(tabsim *tab, char *key);
 
-int ht_retira(tabsim t, char*n);
+int ht_retira(tabsim *tab, char*key);
 
+#endif /* hashtable_h */
