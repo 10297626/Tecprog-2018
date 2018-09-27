72d71a6a337a3bb835a39c184f6ff96439211e17
Henrique Suzuki <32173626+10297626@users.noreply.github.com>
2018-09-27 15:15:51 -0300

    Update advlib.h

--- advlib.h
+++ advlib.h
@@ -5,20 +5,23 @@ Nome: Henrique Suzuki                 NUSP: 10297626
 Nome: Rubens Gomes Neto               NUSP: 9318484
 //////////////////////////////////////////////////////////////////////*/
 
+//#include guards
+#ifndef advlib_h
+#define advlib_h
 
 //Elemento
 typedef struct elemento{
-  char nome[20];//nome do objeto
-  char artigos[4][10];// artigos 
-  char longa[100];//descrição longa para quando examinar o objeto
-  char curta[40];// descrição curta
+	char *nome;//nome do objeto
+//	char *artigos[4][10];// artigos 
+//	char *longa[100];//descrição longa para quando examinar o objeto
+// 	char *curta[40];// descrição curta
 }elemento;
 
 //Objeto
 typedef struct objeto{
-  elemento ele;
-  char adjetivos[][];//adjetivos 
-  int invisivel;// quando 1 o objeto é invisivel
+	elemento *ele;
+//	char *adjetivos[][];//adjetivos 
+	int invisivel;// quando 1 o objeto é invisivel
 }objeto;
 
 void exam(objeto o);// função para examinar o objeto
@@ -30,13 +33,13 @@ void drop(objeto o); // função para soltar o objeto caso ele esteja no invetá
 //Lugares
 
 typedef struct saida{
-  char destino[100];// referencia key do hashtable do destino dessa saida
-  int fechada;// quando 1 a sáida esta fechada
+	char *destino[100];// referencia key do hashtable do destino dessa saida
+	int fechada;// quando 1 a sáida esta fechada
 }saida;
 
 typedef struct lugar {
-  elemento ele;
-  saida saida;// saida do lugar
+	elemento *ele;
+	saida *saida;// saida do lugar
 }lugar;
 
 //imcompletos
@@ -47,4 +50,4 @@ typedef struct lugar {
 //typedef struct{
 //}aventureiro;//o aventureiro
 
-
+#endif /* advlib_h */
