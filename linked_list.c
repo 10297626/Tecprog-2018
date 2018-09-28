2ba89454ef48ef503fb794b2f9509f723bb9862b
Henrique Suzuki <32173626+10297626@users.noreply.github.com>
2018-09-28 10:25:43 -0300

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -5,65 +5,69 @@ Nome: Rubens Gomes Neto               NUSP: 9318484
 //////////////////////////////////////////////////////////////////////*/
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "linked_list.h"
 #include "advlib.h"
 
-lista l_cria(){
-  lista *l= (lista*)malloc(sixeof(lista));
+lista* l_cria(){
+  lista *l= malloc(sizeof(lista));
   l->count=0;
-  l->head=null;
-  l->last=null;
+  l->head=NULL;
   return l;
 }
 
 void l_destroi(lista *l){
+  node *atual=l->head;
+  while (atual!=NULL){
+    free(l->head);
+    l->head=atual;
+    atual=l->head->next;
+  }
+  free(l->head);
   free(l);
 }
 
-lista l_insere(lista l, elemeento *val, char *n){
-  node *new=(node*) malloc(sizeof(node));
+
+lista* l_insere(lista *l, elemento *val){
+  node *new=malloc(sizeof(node));
   new->ele=val;
-  new->next=null;
-  new->nome=n;
-  
-  if (l->head==null){
-    l->head=new;
-    l->last=new;
-  }else{
-    l.last->next=new;
-    l.last=new;
-  }    
+  new->next=l->head;
+  l->head=new;
+  return l;
 }
 
-node l_busca(lista l, char *n){
-  node atual;
-  atual=l->head
-  if(atual.nome==n){
-    return head;
-  }
-  while(atual.next!=null){
-    atual = atual.next;
-    if (atual.nome==n){
-      return atual;
+
+elemento* l_busca(lista *l, char *n){
+  node *atual=l->head;
+
+  while(atual != NULL){
+    char *na=atual->ele->nome;
+    if (strcmp(na,n)==0){
+      return atual->ele;
     }
+    atual = atual->next;
   }
-  return null;      
+  return NULL;      
  }
 
-node l_retira(lista l, char *n){
-  node atual;
-  atual=l->head;
-  if(atual->nome==n){
-    l->head=atual->next;
-    return l->head;
-  }
-  while(atual.next!=null){
-    if(atual.next.nome==n){
-      node rem=atual.next;
-      atual.next=rem.next;
-      rem.next=null;
-      return l->head;
+
+elemento* l_retira(lista *l, elemento *val){
+  node *atual=l->head;
+  node *anterior=atual;
+  while(atual !=NULL){
+    if(strcmp(atual->ele->nome,val->nome)==0){
+      if(atual==l->head){//se for o primeiro da lista
+        l->head=atual->next;
+        atual->next=NULL;
+      }else{ // caso esteja no meio da lista
+        anterior->next=atual->next;
+        atual->next=NULL;
+      }
+      return atual->ele;
     }
+    anterior=atual;
+    atual=atual->next;
   }
-  return null;
+  return NULL;
 }
