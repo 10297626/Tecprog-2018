28207b66d9765f4c3b052996f2276e83b992c388
Henrique Suzuki <32173626+10297626@users.noreply.github.com>
2018-09-28 10:28:01 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -14,38 +14,64 @@ int main(){
 	//teste de hash table
 	//criação da hash table
 	tabsim *tab = ht_cria(30);
-	
+	printf("hash table criada\n");
 	//item exemplo
 	elemento ele;
-	char n;
-	strncpy(&n,"ola",4);
-	ele.nome=&n;
-	char key;
-	strncpy(&key,"alo",4);
+	char n[] = "ola";
+	char *ptrn=n;
+	ele.nome=ptrn;
+	char key[]="alo";
+	char *ptrkey=key;
 
 	//inserção na hash table
-	if(ht_insere(tab, &key, &ele)==1){
-		printf("sucesso na insercao\n");
+	if(ht_insere(tab, ptrkey, &ele)==1){
+		printf("sucesso na insercao da hash table\n");
 	}
 
 	//busca na hash table
-	if(ht_busca(tab,&key)==&ele){
-		printf("sucesso na busca\n");
+	if(ht_busca(tab,ptrkey)==&ele){
+		printf("sucesso na busca da hash table\n");
 	}
 
 	//retirada da hash table
-	if(ht_retira(tab,&key)==1){
-		printf("sucesso na retirada \n");
+	if(ht_retira(tab,ptrkey)==1){
+		printf("sucesso na retirada da hash table \n");
 	}
 	
 	//busca após retirada
-	if(ht_busca(tab,&key)==NULL){
-		printf("item nqo esta na hash table\n");
+	if(ht_busca(tab,ptrkey)==NULL){
+		printf("item nao esta na hash table\n");
 	}
 
 	//destroi a hash table
     ht_destroi(tab);
-
+    printf("hash table destruida\n\n");
 
     //teste da lista ligada
+    //criando a lista
+    lista *l = l_cria();
+   	printf("lista criada\n");
+
+   	//inserindo um elemento
+   	l=l_insere(l,&ele);
+	printf("sucesso na insercao da lista\n");
+
+   	//busca na lista
+   	if (&ele==l_busca(l,ele.nome)){
+   		printf("sucesso na busca da lista\n");
+   	}
+   	
+   	//retirando um elemento
+   	if (&ele==l_retira(l,&ele)){
+   		printf("sucesso na retirada da lista\n");
+   	}
+
+   	//busca na lista
+   	if (l_busca(l,"ola")==NULL){
+   		printf("item nao esta na lista\n");
+   	}
+   	//
+   	l_destroi(l);
+   	printf("lista destruida\n\n");
+   	
 }
