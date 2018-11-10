fe08b8815fe7a877ca0a084886bba7f5b29b4998
Rubens Gomes Neto <rubens.gomes.neto@usp.br>
2018-11-10 01:26:57 -0200

    expansao da defeinicao de elemento, com inclusao de documentacao

--- advlib.h
+++ advlib.h
@@ -7,12 +7,21 @@ Nome: Rubens Gomes Neto               NUSP: 9318484
 #ifndef advlib_h
 #define advlib_h
 
+//Booleanos
+typedef enum {
+False, True
+} Boolean;
 //Elemento
 typedef struct elemento{
-	char *nome;//nome do objeto
-//	char *artigos[4][10];// artigos 
-//	char *longa[100];//descrição longa para quando examinar o objeto
-//	char *curta[40];// descrição curta
+	char *nome; //nome do objeto
+//	char *artigos[4][10]; // artigos
+	char *curta[40]; //descrição curta
+	char *longa[100]; //descrição longa para quando examinar o objeto
+	Boolean ativo; //indica se elemento existe no jogo
+	Boolean visivel; //indicador de visibilidade
+	Boolean conhecido; //indicador de conhecimento do jogador
+//	Lista conteudo; //itens dentro do elemento
+//	Lista acoes; // lista de acoes
 }elemento;
 typedef elemento* Elemento;
 
@@ -22,12 +31,6 @@ typedef struct objeto{
 //	char *adjetivos[][];//adjetivos 
 	int invisivel;// quando 1 o objeto é invisivel
 }objeto;
-
-void exam(objeto o);// função para examinar o objeto
-
-void catch(objeto o);//função para pegar o objeto
-
-void drop(objeto o); // função para soltar o objeto caso ele esteja no invetário
 
 //Lugares
 
@@ -48,5 +51,10 @@ typedef struct lugar {
 
 //typedef struct{
 //}aventureiro;//o aventureiro
+
+void exam(objeto o);// função para examinar o objeto
+void catch(objeto o);//função para pegar o objeto
+void drop(objeto o); // função para soltar o objeto caso ele esteja no invetário
+
 
 #endif /* advlib_h */
