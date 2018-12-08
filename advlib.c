ff7dc9525576472c361047c30e4263056cd429bd
Rubens Gomes Neto <rubens.gomes.neto@usp.br>
2018-12-08 01:38:42 -0200

    edit

--- advlib.c
+++ advlib.c
@@ -370,7 +370,7 @@ struct initlug llugs[] = {
 
 /* Inicializa a tabela de símbolos passada como argumento */
 TabSim init_table(TabSim sym_table) {
-	int i;
+	int i;//int
 	TabSim ptr = sym_table;		/* cabeça da lista */
 	ptr = ht_cria(30);
 
